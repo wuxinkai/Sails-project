443878c5b5cc2f89d70592c7ce3c7335c8824a9f
xinkai.wu@tescomm.com.cn <$ git clone git@101.201.42.201:root/zjxtApp.git>
2017-11-13 17:54:41 +0800

    提交代码

--- Web/scripts/controllers/mainCtrl.js
+++ Web/scripts/controllers/mainCtrl.js
@@ -91,6 +91,28 @@ define(['cookie', //jquery cookie存储
             //处理响应失败
         });
 
+//session存储localStorage存储和cookie存储
+        localStorage.localInfo='{"msg":"登录成功！","success":true,"displayName":"localStorage存储"}';
+        sessionStorage.sessionInfo='{"msg":"登录成功！","success":true,"displayName":"sessionStorage存储"}';
+        var cookie = $.cookie("attr","cookie存储"); //存储
+
+        var loginInfo = localStorage.localInfo || sessionStorage.localInfo;
+        var sessionInfo = localStorage.sessionInfo || sessionStorage.sessionInfo;
+
+
+        if(loginInfo != undefined){
+            $('#localUser').html(JSON.parse(loginInfo).displayName);//ocalStorage存储在页面显示
+        }
+        if(sessionInfo != undefined){
+            $('#sessionUser').html(JSON.parse(sessionInfo).displayName);//ocalStorage存储在页面显示
+        }
+        if(cookie != undefined){
+            $('#cookieUser').html($.cookie('attr')); //cookie读取内容
+        }
+//cookie存储
+//
+
+
 
         function initialMenu(menuList) {
             if (menuList != null && menuList.length > 0) {
